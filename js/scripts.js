27d9453b9a0244ea77afd94a41b4e13d63883c03
Javier Jimenez <javierrcc522@gmail.com>
2017-08-11 11:33:09 -0700

    a function that returns a successful result to the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
   $("form#programmingTrack").submit(function(event) {
     event.preventDefault();
+    var name =$("#userName").val();
+    var system = $("#whichOperatingSystem").val();
+    var developer = $("#userPlatform").val();
+    var company = $("#whichEnviroment").val();
+
+    $("#resultName").text(name + ", from the data you gave us we recomend you to take   ");
+
+    if (system === "1" && developer === "1") {
+      $("#resultTrack").text("C# because it seems to fit you best!!");
+
+    }
 
 
 
 
+
+    $(".well").show();
   });
 });
