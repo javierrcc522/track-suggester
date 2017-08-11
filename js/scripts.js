5242bea67a8c06673850b721cf78d84c62930996
Javier Jimenez <javierrcc522@gmail.com>
2017-08-11 15:07:02 -0700

    add better branches and jumbotron to my web page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $("form#programmingTrack").submit(function(event) {
-    event.preventDefault();
+
     var name =$("#userName").val();
     var system = $("#whichOperatingSystem").val();
     var developer = $("#userPlatform").val();
@@ -8,21 +8,29 @@ $(document).ready(function(){
 
     $("#resultName").text(name + ", from the data you gave us we recomend you to take   ");
 
-    if (system === "1" ) {
+    if (system === "1" || system === "2" ) {
       if (company === "1" && developer === "1") {
-      $("#resultTrack").text("C# because it seems to fit you best!!");
+        $("#resultTrack").text("C# because it seems to fit you best!!");
       }
-      else if (company === "2" && developer === "3") {
-        $("#resultTrack").text("Php# because it seems to fit you best!!");
-       } {
+      else if (company === "2" && developer === "2") {
+        $("#resultTrack").text("Php because it seems to fit you best!!");
+      }
+      else if (company === "3" && developer === "1") {
+        $("#resultTrack").text("Java because it seems to fit you best!!");
+      }else if (company === "2" && developer === "1"){
+        $("#resultTrack").text("Php because it seems to fit you best!!");
+      } else if (company === "2" && developer === "2") {
+        $("#resultTrack").text("Php because it seems to fit you best!!")
+      } else if (company === "3" && developer === "2") {
+        $("#resultTrack").text("Ruby because it seems to fit you best!!")
+      }
 
-       }
+
+
+
+
+      $(".well").toggle();
     }
-
-
-
-
-
-    $(".well").toggle();
+  event.preventDefault();
   });
 });
