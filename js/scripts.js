cddfffe09f45bddc3a83aca6fe072fb4b9138f40
Javier Jimenez <javierrcc522@gmail.com>
2017-08-11 13:19:01 -0700

    php and ruby branches

--- js/scripts.js
+++ js/scripts.js
@@ -10,24 +10,32 @@ $(document).ready(function(){
 
     if (system === "1" && company === "2") {
       $("#resultTrack").text("C# because it seems to fit you best!!");
-
     } else if (system === "1" && developer === "1") {
       $("#resultTrack").text("C# because it seems to fit you best");
-
     }  else if (company === "1" && system === "3") {
       $("#resultTrack").text("C# because it seems to fit you best");
 
-    } else if (company === "1" && developer === "1") {
-      $("#resultTrack").text("C# because it seems to fit you best");
-
-
-
     } else if (system === "2" && developer === "2") {
       $("#resultTrack").text("Java because it seems to fit you best");
-
     } else if (system === "2" && company === "3") {
       $("#resultTrack").text("Java because it seems to fit you best");
+    } else if (system === "2" && developer === "3") {
+      $("#resultTrack").text("Java because it seems to fit you best");
 
+
+    } else if (system === "3" && company === "3") {
+      $("#resultTrack").text("PhP because it seems to fit you best");
+    } else if (system === "3" && company === "2") {
+      $("#resultTrack").text("PhP because it seems to fit you best");
+    } else if (system === "3" && developer === "1") {
+      $("#resultTrack").text("PhP because it seems to fit you best");
+
+    } else if (system === "4" && developer === "2") {
+      $("#resultTrack").text("Ruby because it seems to fit you best");
+    } else if (system === "4" && developer === "3") {
+      $("#resultTrack").text("Ruby because it seems to fit you best");
+    }else if (system === "4" && company === "4") {
+      $("#resultTrack").text("Ruby because it seems to fit you best");
     }
 
 
@@ -35,6 +43,6 @@ $(document).ready(function(){
 
 
 
-    $(".well").show();
+    $(".well").toggle();
   });
 });
