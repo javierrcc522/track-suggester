d97dcd9c1be38175ed0878f7dbc49ad2416966ce
Javier Jimenez <javierrcc522@gmail.com>
2017-08-11 14:13:25 -0700

    add less branches that are better targeted acording to the userinput

--- js/scripts.js
+++ js/scripts.js
@@ -8,36 +8,16 @@ $(document).ready(function(){
 
     $("#resultName").text(name + ", from the data you gave us we recomend you to take   ");
 
-    if (system === "1" && company === "2") {
+    if (system === "1" ) {
+      if (company === "1" && developer === "1") {
       $("#resultTrack").text("C# because it seems to fit you best!!");
-    } else if (system === "1" && developer === "1") {
-      $("#resultTrack").text("C# because it seems to fit you best");
-    }  else if (company === "1" && system === "3") {
-      $("#resultTrack").text("C# because it seems to fit you best");
+      }
+      else if (company === "2" && developer === "3") {
+        $("#resultTrack").text("Php# because it seems to fit you best!!");
+       } {
 
-    } else if (system === "2" && developer === "2") {
-      $("#resultTrack").text("Java because it seems to fit you best");
-    } else if (system === "2" && company === "3") {
-      $("#resultTrack").text("Java because it seems to fit you best");
-    } else if (system === "2" && developer === "3") {
-      $("#resultTrack").text("Java because it seems to fit you best");
-
-
-    } else if (system === "3" && company === "3") {
-      $("#resultTrack").text("PhP because it seems to fit you best");
-    } else if (system === "3" && company === "2") {
-      $("#resultTrack").text("PhP because it seems to fit you best");
-    } else if (system === "3" && developer === "1") {
-      $("#resultTrack").text("PhP because it seems to fit you best");
-
-    } else if (system === "4" && developer === "2") {
-      $("#resultTrack").text("Ruby because it seems to fit you best");
-    } else if (system === "4" && developer === "3") {
-      $("#resultTrack").text("Ruby because it seems to fit you best");
-    }else if (system === "4" && company === "4") {
-      $("#resultTrack").text("Ruby because it seems to fit you best");
+       }
     }
-
 
 
 
